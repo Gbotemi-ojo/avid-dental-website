a0269156e75fda24d7c91f7cd5169e9b44c6ade3
Gbotemi <hommzmum@gmail.com>
2026-09-10 07:39:28 +0100

    removed trailing slash from config api

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 // src/config/api.js
 
-const API_BASE_URL = 'https://avid-dental-emr-be.vercel.app/';
+const API_BASE_URL = 'https://avid-dental-emr-be.vercel.app';
 // const API_BASE_URL = 'http://localhost:5000';
 
 export default API_BASE_URL;
